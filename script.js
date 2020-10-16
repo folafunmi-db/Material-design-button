f3228f0227913a569b1aae54aaffd16433289ad7
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-16 13:41:08 +0100

    Changed button tag to link tag

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 /*jshint esversion: 10 */
 
 function createRipple(event) {
-	const button = event.currentTarget;
+	const a = event.currentTarget;
 
 	const circle = document.createElement("span");
-	const diameter = Math.max(button.clientWidth, button.clientHeight);
+	const diameter = Math.max(a.clientWidth, a.clientHeight);
 	const radius = diameter / 2;
 
 	circle.style.width = circle.style.height = `${diameter}px`;
-	circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-	circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+	circle.style.left = `${event.clientX - a.offsetLeft - radius}px`;
+	circle.style.top = `${event.clientY - a.offsetTop - radius}px`;
 	circle.classList.add("ripple");
 
-	const ripple = button.getElementsByClassName("ripple")[0];
+	const ripple = a.getElementsByClassName("ripple")[0];
 
 	ripple ? ripple.remove() : "";
-  button.appendChild(circle);
+  a.appendChild(circle);
 }
 
-const buttons = document.getElementsByTagName("button");
+const buttons = document.getElementsByTagName("a");
 for (const button of buttons) {
 	button.addEventListener("click", createRipple);
 }
